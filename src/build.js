226b25b9a11529788367c64a23ba44c34f84bd88
Jakob Henner <jakobhenner@gmail.com>
2017-04-04 14:42:03 +0200

    fix export

--- src/build.js
+++ src/build.js
@@ -1,6 +1,6 @@
 import Container from './components/Container'
 import Row from './components/Row'
-import Column from './components/Row'
+import Column from './components/Column'
 
 export { // without default
   Container,
