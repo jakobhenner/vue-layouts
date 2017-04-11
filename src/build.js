1e30a1d6af34b8469ca566918c0b7f03d16f3831
Jakob Henner <jakobhenner@gmail.com>
2017-04-11 10:34:37 +0200

    export layout

--- src/build.js
+++ src/build.js
@@ -1,11 +1,13 @@
 import Container from './components/Container'
 import Row from './components/Row'
 import Column from './components/Column'
+import Layout from './components/Layout'
 
 export {
   Container,
   Row,
-  Column
+  Column,
+  Layout
 }
 
 export { default as breakpoints } from './store/breakpoints'
